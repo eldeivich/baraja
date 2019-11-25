a3fe4cbc29e6bbfbf72db9f6b2a375bbf624ce76
eldeivich <47783040+eldeivich@users.noreply.github.com>
2019-11-25 23:47:43 +0100

    Versión funcional de baraja

---
+++ baraja_test.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest.mock import patch
+import baraja
+
+def mi_eligecarta(i, longitud):
+    if i ==longitud-1:
+        return 0
+    else:
+        return i+1
+
+class BarajaTest(unittest.TestCase):
+
+    def test_crear_baraja(self):
+        b = baraja.baraja()
+        self.assertEqual(len(b), 40)
+        self.assertEqual(b, ['Ao', '2o', '3o', '4o', '5o', '6o', 'So', 'Co', 'Ro', 'Ac', '2c', '3c', '4c', '5c', '6c', 'Sc', 'Cc', 'Rc', 'Ae', '2e', '3e', '4e', '5e', '6e', 'Se', 'Ce', 'Re', 'Ab', '2b', '3b', '4b', '5b', '6b', 'Sb', 'Cb', 'Rb'])
+
+        self.assertEqual(b[0], 'Ao')
+        self.assertEqual(b[39], 'Rb')
+        self.assertEqual(b[10], 'Ac')
+        self.assertEqual(b[20], 'Ao')
+
+    @patch('baraja.elige_carta', mi_eligecarta)
+    def test_mezclar_lista(self):
+        b = ['A', 'B', 'C', 'D', 'E']
+        mezclada = ['A', 'C', 'D', 'E','B']
+        b = baraja.mezclar(b)
+        self.assertEqual(baraja.mezclar(b), mezclada)
+
+if __name__ == '__main__':
+    unittest.main()
